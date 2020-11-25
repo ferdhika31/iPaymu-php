9a467d8c7fd97597da098d9259dfc00b4c339c67
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-11-25 17:23:21 +0700

    :sparkles: example direct payment added

---
+++ examples/DirectQRISExample.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * DirectQRISExample.php
+ *
+ * @author  Ferdhika Yudira
+ * @email   fer@dika.web.id
+ */
+
+require './vendor/autoload.php';
+
+use ferdhika31\iPaymuPHP\iPaymu;
+use ferdhika31\iPaymuPHP\Exceptions\InvalidArgumentException;
+use ferdhika31\iPaymuPHP\Exceptions\InvalidApiKeyException;
+use ferdhika31\iPaymuPHP\PaymentDirect;
+
+$config = [
+    'env'               => 'SANDBOX',
+    'virtual_account'   => 'your_virtual_account',
+    'api_key'           => 'your_api_key',
+    'notify_uri'        => 'http://localhost:8000/notify',
+];
+
+try{
+    iPaymu::init($config);
+
+    $customer = [
+        'name' => 'Dika',
+        'email' => 'fer@dika.web.id',
+        'phone' => '083213123332'
+    ];
+    iPaymu::setCustomer($customer);
+
+
+    $payloadTrx = [
+        'amount' => 5000,
+        // optional
+        'expired' => 10,
+        'expiredType' => 'minutes', // in:seconds,minutes,hours,days
+        'comments' => 'Transaction comment here',
+        'referenceId' => 'TRX202008310001'
+    ];
+
+    // Available channel: linkaja (default)
+    $channel = 'linkaja';
+    $directPayment = PaymentDirect::VA($channel)->create($payloadTrx);
+    var_dump($directPayment);
+}catch (InvalidApiKeyException $e){
+    echo ($e->getMessage());
+}catch (InvalidArgumentException $e){
+    echo ($e->getMessage());
+}
